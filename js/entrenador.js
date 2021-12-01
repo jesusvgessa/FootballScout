fde905b67f49419a641ee5e46f209f576343672a
jesusvgessa <jesusvgessa@hotmail.com>
2021-12-01 13:53:36 +0100

    commit

--- js/entrenador.js
+++ js/entrenador.js
@@ -1,16 +1,56 @@
 //Funcion para desplegable usuario
 function opcionesUser() {
-    document.getElementById("opcionesUsuario").style.display = "block";
+    var menuEmergente = document.getElementById("opcionesUsuario");
+    if (menuEmergente.style.display == "block") {
+        menuEmergente.style.display = "none";
+    } else {
+        menuEmergente.style.display = "block";
+    } //Fin Si
 }
 
 //Funcion para plantilla
-//Si tiene 25 jugadores, no se pueden añadir mas
-window.onload = load;
+//Evento multiple
+function fueraFoco() {
+    var x = document.getElementById("tema");
+    x.value = x.value.toUpperCase();
+    x.style.background = "white";
+}
 
-function load() {
-    var listaJugadores = document.getElementById('jugadores').querySelectorAll('li');
-    console.log(listaJugadores.length);
-    if (listaJugadores.length > 26) {
-        document.getElementById("boton").style.display = "none";
-    } //Fin Si
+function dentroFoco(x) {
+    x.style.background = "orange";
+}
+
+//Objetos
+function Jugador(nombre, dorsal, posicion) {
+    // Propiedades
+    this.nombre = nombre;
+    this.dorsal = dorsal;
+    this.posicion = posicion;
+}
+
+var arrayJugadores = new Array();
+var fichasJugador = document.querySelectorAll(".jugador");
+
+for (var i = 0; i < fichasJugador.length; i++) {
+    var nombre = fichasJugador[i].getElementsByTagName("p")[0].innerHTML;
+    var dorsal = fichasJugador[i].getElementsByTagName("p")[1].innerHTML;
+    var posicion = fichasJugador[i].getElementsByTagName("p")[2].innerHTML;
+    var jugador = new Jugador(nombre, dorsal, posicion);
+    arrayJugadores.push(jugador);
+} //Fin Para
+
+function buscaJugador() {
+    var nombre = document.getElementById("filtroNombre").value;
+    for (var i = 0; i < arrayJugadores.length; i++) {
+        if (!arrayJugadores[i].nombre.toLowerCase().includes(nombre.toLowerCase())) {
+            fichasJugador[i].style.display = "none";
+        } else {
+            fichasJugador[i].style.display = "block";
+        } //Fin Si
+    } //Fin Para
+}
+
+function filtroSelect() {
+    var filtro = document.getElementById("tema").value;
+    //Ordenar por select value
 }
